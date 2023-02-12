951ca62350d4558cf671eff43bdce02356cd3206
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-02-12 14:19:03 +0100

    fixed key listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 const themeRange = document.querySelector('.theme-box__range');
 const theme = document.querySelector('body');
 
-const handleTheme = () => {
-	theme.dataset.theme = `theme-${themeRange.value}`;
+const handleTheme = e => {
+	setTimeout(() => {
+		theme.dataset.theme = `theme-${themeRange.value}`;
+	}, 1);
 };
 
 handleTheme();
 themeRange.addEventListener('click', handleTheme);
+themeRange.addEventListener('keydown', handleTheme);
