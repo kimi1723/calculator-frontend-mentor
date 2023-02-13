dbe5c016d909b50771678208f6770be6cf514e27
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-02-13 01:55:34 +0100

    better functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,9 @@ const themeRange = document.querySelector('.theme-box__range');
 const theme = document.querySelector('body');
 const themeListeners = ['click', 'keydown', 'touchend'];
 const keys = document.querySelectorAll('.calc-keys__key');
-const actionSign = ['+', '-', '/', '*'];
-let numberString = '',
-	number = 0,
-	result = 0,
-	calculations = [],
-	prev = 0,
-	curr = 0;
+const result = document.querySelector('.calc-result__text');
+let value = '';
+let resultV = '';
 
 const handleTheme = () => {
 	setTimeout(() => {
@@ -16,57 +12,29 @@ const handleTheme = () => {
 	}, 1);
 };
 
-const handleResult = key => {
-	key.preventDefault();
+keys.forEach(key => {
+	key.addEventListener('click', e => {
+		e.preventDefault();
 
-	if (key.target.classList.contains('calc-keys__key--number')) {
-		numberString += key.target.textContent;
-		number = parseInt(numberString);
-		console.log(calculations.at[-1]);
-	}
-
-	if (key.target.classList.contains('calc-keys__key--add')) {
-		calculations.push(number, '+');
-		(number = 0), (numberString = '');
-	}
-
-	if (key.target.classList.contains('calc-keys__key--subtract')) {
-		calculations.push(number, '-');
-
-		(number = 0), (numberString = '');
-	}
-
-	if (key.target.classList.contains('calc-keys__key--result')) {
-		calculations.push(number);
-
-		for (let i = 0, j = 1; i < calculations.length; i++, j++) {
-			if (calculations[i] != '+' && calculations[i] != '-') {
-				console.log(actionSign[i]);
-				prev = calculations[i];
-			}
-			if (calculations[j] != '+' && calculations[j] != '-') {
-				curr = calculations[j];
-			}
-
-			switch (calculations[i]) {
-				case '+':
-					result = prev + curr;
-					break;
-				case '-':
-					result = prev - curr;
-					break;
-				default:
-			}
+		if (result.textContent === '0') {
+			result.textContent = '';
 		}
 
-		console.log(result);
-		(number = 0), (numberString = 0), (result = 0), (calculations = []);
-	}
-};
+		if (e.target.textContent !== '=') {
+			let valueString = e.target.textContent;
+			result.textContent += valueString;
+		}
 
+		if (e.target.textContent == '=') {
+			result.textContent = eval(result.textContent);
+		}
+	});
+});
+
+result.textContent = 0;
 handleTheme();
 themeListeners.forEach(listener => {
 	themeRange.addEventListener(listener, handleTheme);
 });
 
-keys.forEach(key => key.addEventListener('click', handleResult));
+// keys.forEach(key => key.addEventListener('click', handleResult));
