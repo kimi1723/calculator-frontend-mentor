1010065a5b2a636569aac49252b45b1584817b63
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-02-13 02:56:25 +0100

    reworked functionality

--- src/js/main.js
+++ src/js/main.js
@@ -12,29 +12,66 @@ const handleTheme = () => {
 	}, 1);
 };
 
-keys.forEach(key => {
-	key.addEventListener('click', e => {
-		e.preventDefault();
+const handleCalc = e => {
+	e.preventDefault();
 
-		if (result.textContent === '0') {
-			result.textContent = '';
-		}
+	if (e.target.textContent !== '=' && e.target.textContent !== 'del' && e.target.textContent !== 'reset') {
+		let valueString = e.target.textContent;
 
-		if (e.target.textContent !== '=') {
-			let valueString = e.target.textContent;
+		if (
+			result.textContent === '0' &&
+			e.target.textContent !== '+' &&
+			e.target.textContent !== '-' &&
+			e.target.textContent !== '/' &&
+			e.target.textContent !== '*' &&
+			e.target.textContent !== '.'
+		) {
+			result.textContent = valueString;
+		} else if (
+			e.target.textContent !== '+' &&
+			e.target.textContent !== '-' &&
+			e.target.textContent !== '/' &&
+			e.target.textContent !== '*' &&
+			e.target.textContent !== '.'
+		) {
 			result.textContent += valueString;
+		} else if (e.target.textContent === '.' && true != result.textContent.includes('.')) {
+			result.textContent += valueString;
+		} else {
+			if (
+				e.target.textContent !== '.' &&
+				result.textContent.at(-1) !== '+' &&
+				result.textContent.at(-1) !== '-' &&
+				result.textContent.at(-1) !== '*' &&
+				result.textContent.at(-1) !== '/'
+			) {
+				result.textContent += valueString;
+			} else if (e.target.textContent !== '.') {
+				result.textContent = result.textContent.slice(0, -1);
+				result.textContent += valueString;
+			}
 		}
+	}
 
-		if (e.target.textContent == '=') {
-			result.textContent = eval(result.textContent);
-		}
-	});
-});
+	if (e.target.textContent === '=') {
+		result.textContent = eval(result.textContent);
+	}
+
+	if (e.target.textContent === 'del' && result.textContent.length > 1) {
+		result.textContent = result.textContent.slice(0, -1);
+	} else if (e.target.textContent === 'del' && result.textContent.length === 1) {
+		result.textContent = '0';
+	}
+
+	if (e.target.textContent === 'reset') {
+		result.textContent = 0;
+	}
+};
 
 result.textContent = 0;
 handleTheme();
+
 themeListeners.forEach(listener => {
 	themeRange.addEventListener(listener, handleTheme);
 });
-
-// keys.forEach(key => key.addEventListener('click', handleResult));
+keys.forEach(key => key.addEventListener('click', handleCalc));
